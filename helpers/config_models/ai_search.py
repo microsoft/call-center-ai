f2bfa5adbe2f27e10763de0d6aeca09b7645fb7c
Clémence Lesné <clemence@lesne.pro>
2024-02-05 13:00:38 +0100

    conf: Add query expansion config

--- helpers/config_models/ai_search.py
+++ helpers/config_models/ai_search.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 class AiSearchModel(BaseSettings):
     access_key: SecretStr
     endpoint: str
+    expansion_k: int = 5
     index: str
     semantic_configuration: str
     top_k: int = 10
