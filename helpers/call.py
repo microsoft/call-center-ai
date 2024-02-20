6959665247d0b340c8663d158976ea9c6b630253
Clémence Lesné <clemence@lesne.pro>
2024-02-20 20:28:38 +0100

    refacto: LLM actions as a dynamic separated file

---
+++ helpers/call.py
@@ -0,0 +1,215 @@
+from enum import Enum
+from helpers.config import CONFIG
+from helpers.logging import build_logger
+from models.call import CallModel
+from models.message import StyleEnum as MessageStyleEnum
+from typing import Generator, List, Optional
+from azure.communication.callautomation import (
+    FileSource,
+    PhoneNumberIdentifier,
+    RecognitionChoice,
+    RecognizeInputType,
+    SsmlSource,
+    CallConnectionClient,
+)
+from azure.core.exceptions import ResourceNotFoundError, HttpResponseError
+from models.message import (
+    MessageModel,
+    PersonaEnum as MessagePersonaEnum,
+    StyleEnum as MessageStyleEnum,
+)
+import re
+
+
+_logger = build_logger(__name__)
+SENTENCE_R = r"[^\w\s+\-–—’/'\",:;()@=]"
+
+
+class ContextEnum(str, Enum):
+    CONNECT_AGENT = "connect_agent"
+    GOODBYE = "goodbye"
+    TRANSFER_FAILED = "transfer_failed"
+
+
+def sentence_split(text: str) -> Generator[str, None, None]:
+    """
+    Split a text into sentences.
+    """
+    separators = re.findall(SENTENCE_R, text)
+    splits = re.split(SENTENCE_R, text)
+    for i, separator in enumerate(separators):
+        local_content = splits[i] + separator
+        yield local_content
+
+
+# TODO: Disable or lower profanity filter. The filter seems enabled by default, it replaces words like "holes in my roof" by "*** in my roof". This is not acceptable for a call center.
+async def _handle_recognize_media(
+    client: CallConnectionClient,
+    call: CallModel,
+    sound_url: str,
+) -> None:
+    """
+    Play a media to a call participant and start recognizing the response.
+    """
+    _logger.debug(f"Recognizing media ({call.call_id})")
+    try:
+        client.start_recognizing_media(
+            end_silence_timeout=3,  # Sometimes user includes breaks in their speech
+            input_type=RecognizeInputType.SPEECH,
+            play_prompt=FileSource(url=sound_url),
+            speech_language=call.lang.short_code,
+            target_participant=PhoneNumberIdentifier(call.phone_number),  # type: ignore
+        )
+    except ResourceNotFoundError:
+        _logger.debug(f"Call hung up before recognizing ({call.call_id})")
+    except HttpResponseError as e:
+        if "call already terminated" in e.message.lower():
+            _logger.debug(f"Call hung up before playing ({call.call_id})")
+        else:
+            raise e
+
+
+async def handle_media(
+    client: CallConnectionClient,
+    call: CallModel,
+    sound_url: str,
+    context: Optional[str] = None,
+) -> None:
+    try:
+        client.play_media(
+            operation_context=context,
+            play_source=FileSource(url=sound_url),
+        )
+    except ResourceNotFoundError:
+        _logger.debug(f"Call hung up before playing ({call.call_id})")
+    except HttpResponseError as e:
+        if "call already terminated" in e.message.lower():
+            _logger.debug(f"Call hung up before playing ({call.call_id})")
+        else:
+            raise e
+
+
+async def handle_recognize_text(
+    client: CallConnectionClient,
+    call: CallModel,
+    style: MessageStyleEnum = MessageStyleEnum.NONE,
+    text: Optional[str] = None,
+    store: bool = True,
+) -> None:
+    """
+    Play a text to a call participant and start recognizing the response.
+
+    If `store` is `True`, the text will be stored in the call messages. Starts by playing text, then the "ready" sound, and finally starts recognizing the response.
+    """
+    if text:
+        await handle_play(
+            call=call,
+            client=client,
+            store=store,
+            style=style,
+            text=text,
+        )
+
+    await _handle_recognize_media(
+        call=call,
+        client=client,
+        sound_url=CONFIG.prompts.sounds.ready(),
+    )
+
+
+async def handle_play(
+    client: CallConnectionClient,
+    call: CallModel,
+    text: str,
+    style: MessageStyleEnum = MessageStyleEnum.NONE,
+    context: Optional[str] = None,
+    store: bool = True,
+) -> None:
+    """
+    Play a text to a call participant.
+
+    If store is True, the text will be stored in the call messages. Compatible with text larger than 400 characters, in that case the text will be split in chunks and played sequentially.
+
+    See: https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=tts
+    """
+    if store:
+        call.messages.append(
+            MessageModel(
+                content=text,
+                persona=MessagePersonaEnum.ASSISTANT,
+                style=style,
+            )
+        )
+
+    # Split text in chunks of max 400 characters, separated by sentence
+    chunks = []
+    chunk = ""
+    for to_add in sentence_split(text):
+        if len(chunk) + len(to_add) >= 400:
+            chunks.append(chunk.strip())  # Remove trailing space
+            chunk = ""
+        chunk += to_add
+    if chunk:
+        chunks.append(chunk)
+
+    try:
+        for chunk in chunks:
+            _logger.info(f"Playing text ({call.call_id}): {text} ({style})")
+            client.play_media(
+                operation_context=context,
+                play_source=_audio_from_text(chunk, style, call),
+            )
+    except ResourceNotFoundError:
+        _logger.debug(f"Call hung up before playing ({call.call_id})")
+    except HttpResponseError as e:
+        if "call already terminated" in e.message.lower():
+            _logger.debug(f"Call hung up before playing ({call.call_id})")
+        else:
+            raise e
+
+
+def _audio_from_text(text: str, style: MessageStyleEnum, call: CallModel) -> SsmlSource:
+    """
+    Generate an audio source that can be read by Azure Communication Services SDK.
+
+    Text requires to be SVG escaped, and SSML tags are used to control the voice. Plus, text is slowed down by 5% to make it more understandable for elderly people. Text is also truncated to 400 characters, as this is the limit of Azure Communication Services TTS, but a warning is logged.
+    """
+    # Azure Speech Service TTS limit is 400 characters
+    if len(text) > 400:
+        _logger.warning(
+            f"Text is too long to be processed by TTS, truncating to 400 characters, fix this!"
+        )
+        text = text[:400]
+    ssml = f"""
+    <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="{call.lang.short_code}">
+        <voice name="{call.lang.voice}" effect="eq_telecomhp8k">
+            <lexicon uri="{CONFIG.resources.public_url}/lexicon.xml" />
+            <mstts:express-as style="{style.value}" styledegree="0.5">
+                <prosody rate="0.95">{text}</prosody>
+            </mstts:express-as>
+        </voice>
+    </speak>
+    """
+    return SsmlSource(ssml_text=ssml.strip())
+
+
+async def handle_recognize_ivr(
+    client: CallConnectionClient,
+    call: CallModel,
+    text: str,
+    choices: List[RecognitionChoice],
+) -> None:
+    _logger.info(f"Playing text before IVR ({call.call_id}): {text}")
+    _logger.debug(f"Recognizing IVR ({call.call_id})")
+    try:
+        client.start_recognizing_media(
+            choices=choices,
+            end_silence_timeout=20,
+            input_type=RecognizeInputType.CHOICES,
+            interrupt_prompt=True,
+            play_prompt=_audio_from_text(text, MessageStyleEnum.NONE, call),
+            speech_language=call.lang.short_code,
+            target_participant=PhoneNumberIdentifier(call.phone_number),  # type: ignore
+        )
+    except ResourceNotFoundError:
+        _logger.debug(f"Call hung up before recognizing ({call.call_id})")
--- models/message.py
+++ models/message.py
@@ -1,7 +1,20 @@
 from datetime import datetime
 from enum import Enum
-from pydantic import BaseModel, Field
-from typing import List
+from pydantic import BaseModel, Field, validator
+from typing import List, Union
+from openai.types.chat import (
+    ChatCompletionAssistantMessageParam,
+    ChatCompletionMessageToolCallParam,
+    ChatCompletionToolMessageParam,
+    ChatCompletionUserMessageParam,
+)
+from inspect import getmembers, isfunction
+from openai.types.chat.chat_completion_chunk import ChoiceDeltaToolCall
+import json
+import re
+
+
+FUNC_NAME_SANITIZER_R = r"[^a-zA-Z0-9_-]"
 
 
 class StyleEnum(str, Enum):
@@ -32,10 +45,65 @@ class PersonaEnum(str, Enum):
 
 
 class ToolModel(BaseModel):
-    content: str
-    function_arguments: str
-    function_name: str
-    tool_id: str
+    content: str = ""
+    function_arguments: str = ""
+    function_name: str = ""
+    tool_id: str = ""
+
+    def to_openai(self) -> ChatCompletionMessageToolCallParam:
+        return ChatCompletionMessageToolCallParam(
+            id=self.tool_id,
+            type="function",
+            function={
+                "arguments": self.function_arguments,
+                "name": "-".join(
+                    re.sub(
+                        FUNC_NAME_SANITIZER_R,
+                        "-",
+                        self.function_name,
+                    ).split("-")
+                ),  # Sanitize with dashes then deduplicate dashes, backward compatibility with old models
+            },
+        )
+
+    @validator("function_name")
+    def validate_function_name(cls, v, values) -> str:
+        if v not in ToolModel._available_function_names():
+            raise ValueError(
+                f'Invalid function names "{v}", available are {ToolModel._available_function_names()}'
+            )
+        return v
+
+    def __add__(self, other: ChoiceDeltaToolCall) -> "ToolModel":
+        if other.id:
+            self.tool_id = other.id
+        if other.function:
+            if other.function.name:
+                self.function_name = other.function.name
+            if other.function.arguments:
+                self.function_arguments += other.function.arguments
+        return self
+
+    async def execute_function(self, plugins: object) -> str:
+        from helpers.logging import build_logger
+
+        logger = build_logger(__name__)
+        name = self.function_name
+        args = json.loads(self.function_arguments)
+
+        try:
+            res = await getattr(plugins, name)(**args)
+            logger.info(f"Executing function {name} ({args}): {res[:50]}...")
+        except Exception as e:
+            res = f"Error executing function {self.function_name}: {e}"
+            logger.warn(res)
+        return res
+
+    @staticmethod
+    def _available_function_names() -> List[str]:
+        from helpers.llm_plugins import LlmPlugins
+
+        return [name for name, _ in getmembers(LlmPlugins, isfunction)]
 
 
 class MessageModel(BaseModel):
@@ -47,3 +115,47 @@ class MessageModel(BaseModel):
     persona: PersonaEnum
     style: StyleEnum = StyleEnum.NONE
     tool_calls: List[ToolModel] = []
+
+    def to_openai(
+        self,
+    ) -> List[
+        Union[
+            ChatCompletionAssistantMessageParam,
+            ChatCompletionToolMessageParam,
+            ChatCompletionUserMessageParam,
+        ]
+    ]:
+        if self.persona == PersonaEnum.HUMAN:
+            return [
+                ChatCompletionUserMessageParam(
+                    content=f"action={self.action.value} style={self.style.value} {self.content}",
+                    role="user",
+                )
+            ]
+
+        elif self.persona == PersonaEnum.ASSISTANT:
+            if not self.tool_calls:
+                return [
+                    ChatCompletionAssistantMessageParam(
+                        content=f"action={self.action.value} style={self.style.value} {self.content}",
+                        role="assistant",
+                    )
+                ]
+
+        res = []
+        res.append(
+            ChatCompletionAssistantMessageParam(
+                content=f"action={self.action.value} style={self.style.value} {self.content}",
+                role="assistant",
+                tool_calls=[tool_call.to_openai() for tool_call in self.tool_calls],
+            )
+        )
+        for tool_call in self.tool_calls:
+            res.append(
+                ChatCompletionToolMessageParam(
+                    content=tool_call.content,
+                    role="tool",
+                    tool_call_id=tool_call.tool_id,
+                )
+            )
+        return res
