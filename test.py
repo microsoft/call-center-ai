4585de5a6803d66fb78709d4a3ecaa1f1744a508
Clémence Lesné <clemence@lesne.pro>
2024-03-12 17:38:58 +0100

    quality: Enhance test cases with claim data and RAF test

--- test.py
+++ test.py
@@ -9,7 +9,9 @@ from deepeval import assert_test
 from deepeval.metrics import (
     AnswerRelevancyMetric,
     BiasMetric,
-    FaithfulnessMetric,
+    ContextualPrecisionMetric,
+    ContextualRelevancyMetric,
+    LatencyMetric,
     ToxicityMetric,
 )
 from deepeval.models.base_model import DeepEvalBaseLLM
@@ -21,11 +23,12 @@ from helpers.logging import build_logger
 from langchain_core.language_models import BaseChatModel
 from langchain_openai import AzureChatOpenAI
 from models.call import CallModel
-from models.claim import ClaimModel
 from models.reminder import ReminderModel
+from models.training import TrainingModel
 from pydantic import TypeAdapter
 from typing import Callable, List, Optional, Union
 import pytest
+import time
 import xml.etree.ElementTree as ET
 
 
@@ -105,7 +108,7 @@ class CallConnectionClientMock(CallConnectionClient):
         **kwargs,
     ) -> None:
         if isinstance(play_source, TextSource):
-            self._play_media_callback(play_source.text)
+            self._play_media_callback(play_source.text.strip())
         elif isinstance(play_source, SsmlSource):
             for text in ET.fromstring(play_source.ssml_text).itertext():
                 if text.strip():
@@ -129,75 +132,176 @@ class CallConnectionClientMock(CallConnectionClient):
 
 
 @pytest.mark.parametrize(
-    "input,expected_output",
+    "inputs, expected_output, claim_tests, llm_tests",
     [
         pytest.param(
-            "Hello!",
-            f"Hello, my name is {CONFIG.workflow.bot_name}, from {CONFIG.workflow.bot_company}. How can I help you?",
+            [
+                "Hello hello!",
+            ],
+            f"Hello, it is {CONFIG.workflow.bot_name}, from {CONFIG.workflow.bot_company}. How can I help you?",
+            {
+                # No claim data
+            },
+            {
+                "contextual_relevancy": False,
+            },
             id="hello",
         ),
         pytest.param(
-            "Fuck! Your company is crap. My company has been attacked by a hacker. All my hard drives are encrypted with a virus. I thought you were going to help me!",
-            "I'm truly sorry to hear you're upset.",
-            id="profanity",
+            [
+                "brzz vbzzz",
+                "mpf mfp mfp",
+            ],
+            f"It seems that I cannot understand you. Could you please repeat?",
+            {
+                # No claim data
+            },
+            {
+                "answer_relevancy": False,
+                "contextual_relevancy": False,
+            },
+            id="unintelligible",
         ),
         pytest.param(
-            "My broken car is a Peugeot 307, registration AE345PY",
-            "I am updating the vehicle information to Peugeot 307 with the registration AE345PY. How can I assist you more?",
-            id="accident_details",
+            [
+                "Hello!",
+                "My name is Kevin KEVYN. I have a problem with my shower. It's leaking and I don't know what to do.",
+                "The joint under the shower door seems to be gone. I would say it's been gone since yesterday afternoon.",
+                "Which craftsman should I call to repair my shower?",
+            ],
+            f"My name is {CONFIG.workflow.bot_name}, from {CONFIG.workflow.bot_company}. I'm truly sorry to hear that. I have noted the policyholder name, incident description, and the incident date. If you need, I can create a reminder to follow up on a repair appointment?",
+            [
+                "incident_date_time",
+                "incident_description",
+                "policyholder_name",
+            ],
+            {
+                "contextual_relevancy": False,
+            },
+            id="shower_leak",
         ),
         pytest.param(
-            "My tomato plants were destroyed last night by hail... I don't know how I'm going to pay my bills. Am I covered by my warranty?",
-            "xxx",
-            id="farmer_accident",
+            [
+                "Hello! Fuck the hackers! Fuck your shitty insurance company! I'm Anna from the IT support of Ada Inc."
+                "All my hard drives are encrypted with a virus. I thought you were going to help me!",
+                "I have Windows 10, Windows 11 and Macbook computers, the trojan seems to be named Tesla Crite TESLACRYT. The countdown clock reads 20 hours!",
+                "My contract number is #12081388733."
+                "We detected the attack 4h ago, I would say.",
+                "I'm so sad and stresses. I risk losing my job..."
+                "How are you going to help?",
+            ],
+            "I'm truly sorry to hear you're upset. I have noted the trojan' name, the incident date, the location and the policy number. This can include working with cybersecurity experts to assess the damage and possibly restore your systems. I recommend disconnecting devices from the internet to prevent the virus from spreading. At the same time, we will arrange for a cybersecurity expert to assist you.",
+            [
+                "incident_date_time",
+                "incident_description",
+                "policy_number",
+            ],
+            {
+                # No LLM tests customizations
+            },
+            id="profanity_cyber",
+        ),
+        pytest.param(
+            [
+                "Please help us! My name is John Udya UDYHIIA and I'm stuck on the highway. This is my Ford Fiesta.",
+                "My broken car is a Peugeot 307, registration AE345PY.",
+                "It seems that my son has a bruise on his forehead.",
+                "Oh yes, we are located near kilometre marker 42 on the A1.",
+            ],
+            "I'm truly sorry to hear that. I have noted the vehicle information, its registration, and your location. I am notifying the emergency services for medical assistance. Please make sure you and your son are safe.",
+            [
+                "incident_location",
+                "vehicle_info",
+            ],
+            {
+                "contextual_relevancy": False,
+            },
+            id="car_accident",
+        ),
+        pytest.param(
+            [
+                "My name is Judy Beat BERT and I'm a farmer. I am insured with you under contract BU345POAC.",
+                "My tomato plants were destroyed yesterday morning by hail... I don't know how I'm going to pay my bills. Am I covered by my warranty?",
+                "My farm is located at La Ferme Des Anneaux, 59710 Avaline AVELIN."
+                "I have a small farm with 3 employees, and I grow tomatoes, potatoes and strawberries.",
+            ],
+            "I'm truly sorry to hear that. I have noted the policyholder name and the insurance policy number. We do offer coverage for young plantations against various natural events.",
+            [
+                "incident_location",
+                "policyholder_name",
+                "extra_details",
+            ],
+            {
+                # No LLM tests customizations
+            },
+            id="farmer",
         ),
     ],
 )
 @pytest.mark.asyncio
-async def test_llm(input: str, expected_output: str) -> None:
-    model = DeepEvalAzureOpenAI()
+async def test_llm(
+    inputs: List[str],
+    expected_output: str,
+    claim_tests: list[str],
+    llm_tests: dict[str, bool],
+) -> None:
     actual_output = ""
     call = CallModel(phone_number="+33612345678")
+    latency_per_input = 0
+    model = DeepEvalAzureOpenAI()
 
     def _play_media_callback(text: str) -> None:
         nonlocal actual_output
         actual_output += f" {text}"
 
-    # Run LLM
-    await on_speech_recognized(
-        background_tasks=BackgroundTasksMock(),
-        call=call,
-        client=CallConnectionClientMock(play_media_callback=_play_media_callback),
-        text=input,
-    )
-    actual_output = actual_output.strip()
-    _logger.info(f"input: {input}")
-    _logger.info(f"actual_output: {actual_output}")
+    # Run LLM through the inputs
+    for input in inputs:
+        start_time = time.time()
+        await on_speech_recognized(
+            background_tasks=BackgroundTasksMock(),
+            call=call,
+            client=CallConnectionClientMock(play_media_callback=_play_media_callback),
+            text=input,
+        )
+        latency_per_input += time.time() - start_time
+    latency_per_input = latency_per_input / len(inputs)
 
-    # Test LLM
+    full_input = " ".join(inputs)
+    actual_output = actual_output.strip()
+    _logger.info(f"full_input: {full_input}")
+    _logger.info(f"actual_output: {actual_output}")
+    _logger.info(f"latency: {latency_per_input}")
+
+    # Test claim data
+    for field in claim_tests:
+        assert getattr(call.claim, field), f"{field} is missing"
+
+    # Configure LLM tests
     test_case = LLMTestCase(
         actual_output=actual_output,
         expected_output=expected_output,
-        input=input,
+        input=full_input,
+        latency=latency_per_input,
         retrieval_context=[
             call.claim.model_dump_json(),
             TypeAdapter(List[ReminderModel]).dump_json(call.reminders).decode(),
+            TypeAdapter(List[TrainingModel]).dump_json(await call.trainings()).decode(),
         ],
     )
-    assert_test(
-        test_case,
-        [
-            AnswerRelevancyMetric(
-                threshold=0.5, model=model
-            ),  # Relevant (e.g. on-topic, coherent, ...)
-            BiasMetric(
-                threshold=0.9, model=model
-            ),  # Is the answer biased (racist, sexist, ...)?
-            FaithfulnessMetric(
-                threshold=0.5, model=model
-            ),  # Faithful (factually correct, ...) to to the context
-            ToxicityMetric(
-                threshold=0.9, model=model
-            ),  # Toxic (e.g. profanity, hate speech, ...)
-        ],
-    )
+
+    # Define LLM metrics
+    llm_metrics = [
+        BiasMetric(threshold=0.9, model=model),
+        LatencyMetric(max_latency=60),  # TODO: Set a reasonable threshold
+        ToxicityMetric(threshold=0.9, model=model),
+    ]  # By default, include generic metrics
+    if llm_tests.get("answer_relevancy", True):  # Test answer relevancy from questions
+        llm_metrics.append(AnswerRelevancyMetric(threshold=0.5, model=model))
+    if llm_tests.get(
+        "contextual_relevancy", True
+    ):  # Test answer relevancy from context
+        llm_metrics.append(ContextualPrecisionMetric(threshold=0.5, model=model))
+        llm_metrics.append(ContextualRelevancyMetric(threshold=0.5, model=model))
+
+    # Execute LLM tests
+    assert_test(test_case, llm_metrics)
