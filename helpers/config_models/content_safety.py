02562af380fec24d7d400dfca099ee98cbbc2a59
Clémence Lesné <clemence@lesne.pro>
2024-06-20 17:51:01 +0200

    fix: Lower default Content Safety self harm trigger
    With 1/7, it triggers as soon as we talk about an accident. So, disable by default.

--- helpers/config_models/content_safety.py
+++ helpers/config_models/content_safety.py
@@ -5,7 +5,7 @@ class ContentSafetyModel(BaseModel):
     access_key: SecretStr
     blocklists: list[str] = []
     category_hate_score: int = Field(default=0, ge=0, le=7)
-    category_self_harm_score: int = Field(default=1, ge=0, le=7)
+    category_self_harm_score: int = Field(default=0, ge=0, le=7)
     category_sexual_score: int = Field(default=2, ge=0, le=7)
     category_violence_score: int = Field(default=0, ge=0, le=7)
     endpoint: str
