9c3908170fa726b2ad5157f2d629d632857ee48f
Clémence Lesné <clemence@lesne.pro>
2024-06-21 15:15:50 +0200

    security: Upgrade deps

--- tests/llm.py
+++ tests/llm.py
@@ -42,6 +42,7 @@ class ClaimRelevancyMetric(BaseMetric):
         model: GPTModel,
         threshold: float = 0.5,
     ):
+        super().__init__()
         self.call = call
         self.model = model
         self.threshold = threshold
@@ -203,7 +204,8 @@ class ClaimRelevancyMetric(BaseMetric):
         return {
             extract["key"]: extract["value"]
             for extract in extracts
-            if extract["value"]
+            if "value" in extract
+            and "key" in extract
             and any(
                 claim_field.name == extract["key"]
                 for claim_field in self.call.initiate.claim
