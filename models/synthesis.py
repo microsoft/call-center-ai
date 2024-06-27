0f10709a5c6ea6881664ec10f32300555c7fff3e
Clémence Lesné <clemence@lesne.pro>
2024-06-27 14:41:54 +0200

    feat: Add satisfaction analysis after call

--- models/synthesis.py
+++ models/synthesis.py
@@ -1,4 +1,13 @@
+from enum import Enum
 from pydantic import BaseModel, Field
+
+
+class SatisfactionEnum(str, Enum):
+    TERRIBLE = "terrible"  # 1
+    LOW = "low"  # 2
+    PARTIAL = "partial"  # 3
+    HIGH = "high"  # 4
+    UNKNOW = "unknow"
 
 
 class SynthesisModel(BaseModel):
@@ -15,6 +24,9 @@ class SynthesisModel(BaseModel):
         - Use Markdown syntax to format the message with paragraphs, bold text, and URL
         """
     )
+    satisfaction: SatisfactionEnum = Field(
+        description="How satisfied is the customer with the call."
+    )
     short: str = Field(
         description="""
         Summarize the call with the customer in a few words. The customer cannot reply to this message, but will read it in their web portal.
@@ -28,5 +40,14 @@ class SynthesisModel(BaseModel):
         - "the theft of your car"
         - "the water damage in your kitchen"
         - "your broken window"
+        """
+    )
+    improvement_suggestions: str = Field(
+        description="""
+        Provide suggestions to improve the customer experience during the call.
+
+        # Rules
+        - Include suggestions to improve the call process, the assistant's behavior, or the company's service
+        - No more than a few sentences
         """
     )
