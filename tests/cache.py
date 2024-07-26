9ad7dabdeb71b126d03201e03e5a2dc0d3efc7e7
Clémence Lesné <clemence@lesne.pro>
2024-07-26 16:03:02 +0000

    test: Enance cache tests by testing full ACID

--- tests/cache.py
+++ tests/cache.py
@@ -29,6 +29,8 @@ async def test_acid(random_text: str, cache_mode: CacheModeEnum) -> None:
     2. Test not exists
     3. Insert test data
     4. Check it exists
+    5. Delete data
+    6. Check it doesn't exist anymore
 
     Test is repeated 10 times to catch multi-threading and concurrency issues.
     """
@@ -43,8 +45,14 @@ async def test_acid(random_text: str, cache_mode: CacheModeEnum) -> None:
     # Check not exists
     assume(not await cache.aget(test_key))
 
-    # Insert test call
+    # Insert test key
     await cache.aset(test_key, test_value)
 
     # Check point read
     assume(await cache.aget(test_key) == test_value.encode())
+
+    # Delete test key
+    await cache.adel(test_key)
+
+    # Check not exists
+    assume(not await cache.aget(test_key))
