ee3c2d7ce808655864704c0166a017357df56e2d
Clémence Lesné <clemence@lesne.pro>
2024-11-07 15:32:43 +0100

    fix: Display all logs in the console
    Not only in App Insights

--- app/helpers/logging.py
+++ app/helpers/logging.py
@@ -1,7 +1,8 @@
-from logging import basicConfig, getLogger
+from logging import StreamHandler, basicConfig, getLogger
 
 from app.helpers.config import CONFIG
 
 basicConfig(level=CONFIG.monitoring.logging.sys_level.value)
+getLogger("").addHandler(StreamHandler())
 logger = getLogger("call-center-ai")
 logger.setLevel(CONFIG.monitoring.logging.app_level.value)
